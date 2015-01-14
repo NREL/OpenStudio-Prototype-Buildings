5bbda1e6c3f02252d058f08ed6ef08c2682c0037
yixingchen <yixingchen@lbl.gov>
2015-01-13 17:48:13 -0800

    Add the legacy prototype and reference idf files for large hotel Slightly update the Prototype.strip_model.rb to strip model for a list of files

--- create_DOE_prototype_building/resources/Prototype.strip_model.rb
+++ create_DOE_prototype_building/resources/Prototype.strip_model.rb
@@ -1,13 +1,18 @@
-
 require 'openstudio'
-require_relative 'utilities'
+require_relative 'prototype.utilities'
 
-model = safe_load_model("C:/GitRepos/OpenStudio-Prototype-Buildings/create_DOE_prototype_building/resources/prototype data/small_office_pret_1980_raw.osm")
+folder = "C:/Users/yixing/Dropbox/LBNL/10.OSM reference buildings/Hotel Large/IDF for geometry/"
 
-model = strip_model(model)
+filenames = ["ASHRAE90.1_HotelLarge_STD2007_San_Francisco", "ASHRAE90.1_HotelLarge_STD2010_San_Francisco", "ASHRAE90.1_HotelLarge_STD2013_San_Francisco","RefBldgLargeHotelPost1980_v1.4_7.2_3C_USA_CA_SAN_FRANCISCO"]
 
-new_path = OpenStudio::Path.new("#{Dir.pwd}/stripped_model.osm")
+filenames.each do |filename|
+  model = safe_load_model(folder + filename + ".osm")
 
-model.save(new_path, true)
+  model = strip_model(model)
 
-puts "Stripped model was saved to #{new_path}"
+  new_path = OpenStudio::Path.new(filename + ".osm")
+  model.save(new_path, true)
+
+  puts "Stripped model was saved to #{filename}.osm"
+end
+
