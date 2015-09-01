500735dfb6647e7dadc7076a3080cffdb41c30ba
phylroy <phylroy.lopez@gmail.com>
2015-09-01 12:39:51 -0400

    Added file to run all tests.

---
+++ lib/btap/measures/test_all.rb
@@ -0,0 +1,7 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+require "#{File.dirname(__FILE__)}/apply_system1/test/CanadianAddUnitaryAndApplyStandard_test.rb"
+require "#{File.dirname(__FILE__)}/btap_equest_converter/tests/btap_equest_converter_test.rb"
+
