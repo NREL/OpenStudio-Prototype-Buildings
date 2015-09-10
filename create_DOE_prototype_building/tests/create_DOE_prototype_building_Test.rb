a15d31554586374c3ccdfb4285b4ead9c586bec5
Kaiyu Sun <ksun@lbl.gov>
2015-09-10 16:14:09 -0700

    commit before merge

--- create_DOE_prototype_building/tests/create_DOE_prototype_building_Test.rb
+++ create_DOE_prototype_building/tests/create_DOE_prototype_building_Test.rb
@@ -989,26 +989,26 @@ class CreateDOEPrototypeBuildingTest < Minitest::Unit::TestCase
   if hostname == "SRG-SKY" 
     def test_small_hotel_ptool
       bldg_types = ['SmallHotel']
-      # vintages = ['90.1-2004'] 
+      # vintages = ['DOE Ref 1980-2004', 'DOE Ref Pre-1980','90.1-2004'] 
       vintages = ['DOE Ref 1980-2004', 'DOE Ref Pre-1980', '90.1-2007', '90.1-2010', '90.1-2013', '90.1-2004'] 
        # climate_zones = ['ASHRAE 169-2006-1A', 'ASHRAE 169-2006-1B', 'ASHRAE 169-2006-2A','ASHRAE 169-2006-2B',
                         # 'ASHRAE 169-2006-3A', 'ASHRAE 169-2006-3B', 'ASHRAE 169-2006-3C', 'ASHRAE 169-2006-4A',
                         # 'ASHRAE 169-2006-4B', 'ASHRAE 169-2006-4C', 'ASHRAE 169-2006-5A', 'ASHRAE 169-2006-5B',
                         # 'ASHRAE 169-2006-5C', 'ASHRAE 169-2006-6A', 'ASHRAE 169-2006-6B', 'ASHRAE 169-2006-7A',
                         # 'ASHRAE 169-2006-8A', 'ASHRAE 169-2006-8B']
-       climate_zones = ['ASHRAE 169-2006-1A', 'ASHRAE 169-2006-2A','ASHRAE 169-2006-2B',
-                        'ASHRAE 169-2006-3A', 'ASHRAE 169-2006-3B', 'ASHRAE 169-2006-3C', 'ASHRAE 169-2006-4A',
-                        'ASHRAE 169-2006-4B', 'ASHRAE 169-2006-4C', 'ASHRAE 169-2006-5A', 'ASHRAE 169-2006-5B',
-                        'ASHRAE 169-2006-6A', 'ASHRAE 169-2006-6B', 'ASHRAE 169-2006-7A', 'ASHRAE 169-2006-8A'] 
-      # climate_zones = ['ASHRAE 169-2006-3C']  # for test
+       # climate_zones = ['ASHRAE 169-2006-1A', 'ASHRAE 169-2006-2A','ASHRAE 169-2006-2B',
+                        # 'ASHRAE 169-2006-3A', 'ASHRAE 169-2006-3B', 'ASHRAE 169-2006-3C', 'ASHRAE 169-2006-4A',
+                        # 'ASHRAE 169-2006-4B', 'ASHRAE 169-2006-4C', 'ASHRAE 169-2006-5A', 'ASHRAE 169-2006-5B',
+                        # 'ASHRAE 169-2006-6A', 'ASHRAE 169-2006-6B', 'ASHRAE 169-2006-7A', 'ASHRAE 169-2006-8A'] 
+      climate_zones = ['ASHRAE 169-2006-3C']  # for test
 
       all_failures = []
 
       # Create the models
-      # all_failures += create_models(bldg_types, vintages, climate_zones)
+      all_failures += create_models(bldg_types, vintages, climate_zones)
 
       # Run the models
-      # all_failures += run_models(bldg_types, vintages, climate_zones)
+      all_failures += run_models(bldg_types, vintages, climate_zones)
 
       # Compare the results to the legacy idf results
       all_failures += compare_results(bldg_types, vintages, climate_zones)
