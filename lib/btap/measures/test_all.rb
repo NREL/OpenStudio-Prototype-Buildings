5e9bd6849967e9488821907c2b9b10e7cce451e5
phylroy <phylroy.lopez@gmail.com>
2015-09-21 23:34:23 -0400

    Added native ruby script to convert xls2csv

--- lib/btap/measures/test_all.rb
+++ lib/btap/measures/test_all.rb
@@ -1,14 +1,14 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
-
+t1 = Time.now
 require "#{File.dirname(__FILE__)}/apply_system1/test/CanadianAddUnitaryAndApplyStandard_test.rb"
 require "#{File.dirname(__FILE__)}/btap_equest_converter/test/btap_equest_converter_test.rb"
 require "#{File.dirname(__FILE__)}/btap_change_building_location/test/btap_change_location_test.rb"
 require "#{File.dirname(__FILE__)}/btap_set_default_construction_set/tests/set_default_construction_set_test.rb"
 require "#{File.dirname(__FILE__)}/btap_replace_model/tests/replacemodel_test.rb"
 require "#{File.dirname(__FILE__)}/UtilityTariffs/tests/UtilityTariffs_Test.rb"
-
+puts "#{(Time.now - t1)/60} minutes"
 
 
 
